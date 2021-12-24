7a9a85cf9cf9ed34648c3874316e01d7e06eb28f
dirk <info@universalappfactory.de>
2021-12-24 14:02:37 +0100

    - added a max_download_size_mb config param to prevent downloading to large files - modifed text convert

--- matrix_photos/photos_client.py
+++ matrix_photos/photos_client.py
@@ -19,6 +19,7 @@ from mautrix.crypto.attachments.attachments import decrypt_attachment
 from mautrix.client.state_store.sqlalchemy import SQLStateStore as BaseSQLStateStore
 from mautrix.types.event.encrypted import EncryptedEvent
 from mautrix.types.event.message import MediaMessageEventContent, MessageType, TextMessageEventContent
+from mautrix.types.filter import Filter
 from mautrix.types.misc import PaginationDirection
 from mautrix.types.primitive import RoomID, UserID
 from mautrix.util.async_db import Database as AsyncDatabase
@@ -65,6 +66,7 @@ class PhotOsClient():
 
         self.text_message_command_handler = TextmessageCommandHandler(config, logger)
         self.random_response_messages = get_config_value(config, "random_response_messages", False)
+        self.max_download_size_mb = get_config_value(config, "max_download_size_mb")
 
         self.crypto_db = None
         self.client = None
@@ -153,7 +155,15 @@ class PhotOsClient():
     def is_admin_user(self, user_id: UserID) -> bool:
         return user_id == self.admin_user
 
-    async def _store_data(self, media_content: MediaMessageEventContent) -> None:
+    def max_download_size_exceeded(self, media_content: MediaMessageEventContent) -> bool:
+        media_size = media_content.info.size
+        return self.max_download_size_mb < media_size / (1024*1024)
+
+    async def _store_data(self, media_content: MediaMessageEventContent) -> bool:
+        if self.max_download_size_exceeded(media_content):
+            self.log.warn('max download size exceeded')
+            return False
+
         encrypted_data = await self.client.download_media(media_content.file.url)
 
         file_hash = media_content.file.hashes['sha256']
@@ -162,6 +172,7 @@ class PhotOsClient():
 
         #IDEA maybe store the hash somewhere and only store the file if we dont have a file with the same hash
         self.storage_strategy.store(decrypted_data, str(media_content.body))
+        return True
 
 
     def _is_allowed_content(self, content: MediaMessageEventContent):
@@ -189,19 +200,24 @@ class PhotOsClient():
             await self._handle_admin_command(evt)
         else:
             is_foreign_message = evt.sender != self.user_id
-            media_message_before = await self.message_before_was_media_message(evt.room_id)
+            media_message_before = await self.message_before_was_media_message(evt.room_id, evt.sender)
             if is_foreign_message and media_message_before:
                 self.text_message_command_handler.handle(evt.content)
 
 
-    async def message_before_was_media_message(self, room_id: RoomID) -> bool:
+    async def message_before_was_media_message(self, room_id: RoomID, sender_id: UserID) -> bool:
         token = await self.client.sync_store.get_next_batch()
         if token:
-            messages = await self.client.get_messages(room_id, direction=PaginationDirection.BACKWARD, from_token=token, limit=10)
-            foreign_messages = list(filter(lambda x: x.sender != self.user_id, messages.events))[:2]
+            # TODO I think there is a bug in the mautrix framework, the filter_json query parameter should be filter
 
-            if len(foreign_messages) == 2:
-                event = foreign_messages[1]
+            # sender_filter = f'{{"lazy_load_members":true,"limit":2,"senders":["{sender_id}"],"not_senders":["{self.user_id}"]}}'
+            # self.log.trace(sender_filter)
+
+            messages = await self.client.get_messages(room_id, direction=PaginationDirection.BACKWARD, from_token=token, limit=20)
+            sender_messages = list(filter(lambda x: x.sender == sender_id, messages.events))[:2]
+
+            if len(sender_messages) == 2:
+                event = sender_messages[1]
                 if isinstance(event, EncryptedEvent):
                     decrypted = await self.client.crypto.decrypt_megolm_event(event)
                     if decrypted and isinstance(decrypted.content, MediaMessageEventContent):
@@ -217,6 +233,11 @@ class PhotOsClient():
         content.set_reply(evt)
         await self.client.send_message_event(evt.room_id, EventType.ROOM_MESSAGE, content)
 
+    async def _send_reply_text_message(self, evt: StrippedStateEvent, response_text: str):
+        content = TextMessageEventContent(MessageType.TEXT, response_text)
+        content.set_reply(evt)
+        await self.client.send_message_event(evt.room_id, EventType.ROOM_MESSAGE, content)
+
     async def _handle_message(self, evt: StrippedStateEvent) -> None:
         self.log.trace('_handle_message')
 
@@ -227,8 +248,10 @@ class PhotOsClient():
 
             if isinstance(evt.content, MediaMessageEventContent) and self._is_allowed_content(evt.content):
                 self.log.trace('MediaMessageEventContent')
-                await self._store_data(evt.content)
-                await self._send_random_response_message(evt)
+                if await self._store_data(evt.content):
+                    await self._send_random_response_message(evt)
+                else:
+                    await self._send_reply_text_message(evt, "your file has been revoked.")
 
         #pylint: disable=broad-except
         except Exception as error:
